0632da1ea5b35817a0de6eb97c8813e9297ac326
Carlos Eduardo Witte <cewitte@icloud.com>
2025-04-20 21:34:27 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Carlos Eduardo Witte on 20/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
